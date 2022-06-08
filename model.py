a8f59ca2e0cae6ef05a0bdc3bb5715cc03997e89
idergunoff <slon9124>
2022-06-08 13:30:48 +0300

    модель базы данных

--- model.py
+++ model.py
@@ -12,7 +12,61 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
 
+    telegram_id = Column(BigInteger, primary_key=True)
+    name = Column(String)
+
+    events = relationship('Event', back_populates='user')
+
+
+class Event(Base):
+    __tablename__ = 'event'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, ForeignKey('user.telegram_id'))
+    title = Column(String)
+
+    user = relationship('User', back_populates='events')
+    participants = relationship('Participant', back_populates='event')
+
+
+class Participant(Base):
+    __tablename__ = 'participant'
+
+    id = Column(Integer, primary_key=True)
+    event_id = Column(Integer, ForeignKey('event.id'))
+    name = Column(String)
+    spent = Column(Float, default=0)
+    debt = Column(Float, default=0)
+
+    event = relationship('Event', back_populates='participants')
+    expenses = relationship('Expense', back_populates='participant')
+    exclusions = relationship('Exclusion', back_populates='participant')
+
+
+class Expense(Base):
+    __tablename__ = 'expense'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    price = Column(Float)
+    participant_id = Column(Integer, ForeignKey('participant.id'))
+
+    participant = relationship('Participant', back_populates='expenses')
+    exclusions = relationship('Exclusion', back_populates='expense')
+
+
+class Exclusion(Base):
+    __tablename__ = 'exclusion'
+
+    id = Column(Integer, primary_key=True)
+    participant_id = Column(Integer, ForeignKey('participant.id'))
+    expense_id = Column(Integer, ForeignKey('expense.id'))
+
+    participant = relationship('Participant', back_populates='exclusions')
+    expense = relationship('Expense', back_populates='exclusions')
 
 
 Base.metadata.create_all(engine)
